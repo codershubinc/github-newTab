9687a1a3b97ac1ab8fee74dbfbc276ffa0ab8f58
Swapnil Ingle <ingleswapnil2004@gmail.com>
2025-04-03 23:56:40 +0530

    fix: update excluded repository in GitHub stats URL for top languages

--- extensionsWithVite/GithubNewTab/src/pages/newtab/Newtab.tsx
+++ extensionsWithVite/GithubNewTab/src/pages/newtab/Newtab.tsx
@@ -45,7 +45,7 @@ export default function Newtab() {
                 initial={{ opacity: 0, x: -50 }}
                 animate={{ opacity: 1, x: 0 }}
                 transition={{ duration: 0.7 }}
-                src={`https://github-readme-stats.vercel.app/api/top-langs/?username=${githubUser}&exclude_repo=DJango&langs_count=10&layout=donut&theme=radical&nocache=${new Date().getTime()}`}
+                src={`https://github-readme-stats.vercel.app/api/top-langs/?username=${githubUser}&exclude_repo=R-lang&langs_count=10&layout=donut&theme=radical&nocache=${new Date().getTime()}`}
                 alt="Top Languages"
                 className="w-[500px] h-[540px] rounded-lg shadow-md hover:scale-95 transition-transform duration-300"
               />
