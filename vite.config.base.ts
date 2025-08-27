87f4e7144c3fce0930817d0f9077054f764fecae
Swapnil Ingle <ingleswapnil2004@gmail.com>
2025-08-28 00:38:02 +0530

    Refactor code structure for improved readability and maintainability

--- vite.config.base.ts
+++ vite.config.base.ts
@@ -6,7 +6,6 @@ import { defineConfig, BuildOptions } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { stripDevIcons, crxI18n } from './custom-vite-plugins';
 import manifest from './manifest.json';
-import devManifest from './manifest.dev.json';
 import pkg from './package.json';
 
 
@@ -15,14 +14,13 @@ const isDev = process.env.__DEV__ === 'true';
 const localize = false;
 
 export const baseManifest = {
-    ...manifest,
-    version: pkg.version,
-    ...(isDev ? devManifest : {} as ManifestV3Export),
-    ...(localize ? {
-      name: '__MSG_extName__',
-      description: '__MSG_extDescription__',
-      default_locale : 'en'
-    } : {})
+  ...manifest,
+  version: pkg.version,
+  ...(localize ? {
+    name: '__MSG_extName__',
+    description: '__MSG_extDescription__',
+    default_locale: 'en'
+  } : {})
 } as ManifestV3Export
 
 export const baseBuildOptions: BuildOptions = {
